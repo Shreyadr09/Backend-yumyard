91a36c002739902f57f933082bb60fc5cf6cf700
Shreya DR <shreyadr09@gmail.com>
2024-03-05 21:36:01 +0530

    image link one

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const products = [
         price: 109.95,
         description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
         category: "men's clothing",
-        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        image: "https://media.istockphoto.com/id/1488737992/photo/upma-recipe-suji-ka-upma-rava-upma-with-red-and-coconut-chutney.jpg?s=1024x1024&w=is&k=20&c=u-v3YdnX80PL_xzAKo6nLqZARkkBKfTNI_Z1j3xdR7Y=",
         rating: {
             rate: 3.9,
             count: 120
